86f2f072906f3dcd50674af1fef7c34875c3b4f0
Ascendise <rapperido@gmail.com>
2023-05-10 16:35:42 +0200

    fake texture for testing sprite

---
+++ tests/rendering/test_doubles/fakes/fake_texture.cpp
@@ -0,0 +1,25 @@
+#ifndef FAKE_TEXTURE_CPP
+#define FAKE_TEXTURE_CPP
+
+#include <rendering/rendering.hpp>
+
+namespace pong {
+    namespace rendering {
+        class FakeTexture : ITexture {
+        private:
+            SDL_Rect size;
+        public:
+            FakeTexture(SDL_Rect size) {
+                this->size = size;
+            }
+            SDL_Texture* getSDLTexture() {
+                return nullptr;
+            };
+            SDL_Rect getTextureSize() {
+                return size;
+            };
+        }
+    }
+}
+
+#endif
