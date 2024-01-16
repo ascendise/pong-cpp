003c9fb4f4985e0120f599ed1bf7df81a0aa3144
Ascendise <rapperido@gmail.com>
2024-01-16 23:00:19 +0100

    setup clang tidy

--- lib/includes/components.hpp
+++ lib/includes/components.hpp
@@ -1,19 +1,15 @@
 #ifndef COMPONENTS_HPP
 #define COMPONENTS_HPP
 
-#include "world.hpp"
+#include <world.hpp>
 #include <math.hpp>
 
-namespace pong {
-	namespace world {
+namespace pong::world {
 
-		class Position : public Component, public math::Vector2D 
-		{
-		public:
-			Position(int x, int y) : math::Vector2D(x, y) { }
-		};
-
-	}
+	class Position : public Component, public math::Vector2D {
+	public:
+		Position(float x, float y) : math::Vector2D(x, y) { }
+	};
 }
 
 #endif
--- lib/includes/events.hpp
+++ lib/includes/events.hpp
@@ -5,45 +5,41 @@
 #include <queue>
 #include <memory>
 
-namespace pong {
-    namespace world {
-        namespace events {
+namespace pong::world::events {
 
-            class Event {
-                public:
-                    virtual ~Event() {}
-            };
+    class Event {
+    public:
+        virtual ~Event() {}
+    };
 
-            class EventProcessor {
-                public:
-                    virtual void process(Event& event) = 0;
-                    virtual ~EventProcessor() {}
-            };
+    class EventProcessor {
+    public:
+        virtual void process(Event& event) = 0;
+        virtual ~EventProcessor() {}
+    };
 
-            class IEventQueuePort {
-            public:
-                virtual void enqueue(std::shared_ptr<Event>&& event) = 0;
-            };
+    class IEventQueuePort {
+    public:
+        virtual void enqueue(std::shared_ptr<Event>&& event) = 0;
+    };
 
-            class IEventQueue : public IEventQueuePort{
-                public:
-                    virtual void registerProcessor(std::unique_ptr<EventProcessor>&& processor) = 0;
-                    virtual void processEvents() = 0;
-                    virtual ~IEventQueue() {}
-            };
+    class IEventQueue : public IEventQueuePort {
+    public:
+        virtual void registerProcessor(std::unique_ptr<EventProcessor>&& processor) = 0;
+        virtual void processEvents() = 0;
+        virtual ~IEventQueue() {}
+    };
 
-            class EventQueue : public IEventQueue{
-                private:
-                    std::queue<std::shared_ptr<Event>> events;
-                    std::vector<std::unique_ptr<EventProcessor>> processors;
-                public:
-                    EventQueue() {}
-                    void registerProcessor(std::unique_ptr<EventProcessor>&& processor);
-                    void enqueue(std::shared_ptr<Event>&& event);
-                    void processEvents();
-            };
-        }
-    }
+    class EventQueue : public IEventQueue {
+    private:
+        std::queue<std::shared_ptr<Event>> events;
+        std::vector<std::unique_ptr<EventProcessor>> processors;
+    public:
+        EventQueue() {}
+        void registerProcessor(std::unique_ptr<EventProcessor>&& processor);
+        void enqueue(std::shared_ptr<Event>&& event);
+        void processEvents();
+    };
 }
 
 #endif
