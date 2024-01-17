7334f13502416aefad93c614a95713679d650436
Ascendise <rapperido@gmail.com>
2024-01-17 01:49:49 +0100

    cleanup event stuff

--- lib/includes/events.hpp
+++ lib/includes/events.hpp
@@ -9,25 +9,26 @@ namespace pong::world::events {
 
     class Event {
     public:
-        virtual ~Event() {}
+        virtual ~Event() = default;
     };
 
     class EventProcessor {
     public:
         virtual void process(Event& event) = 0;
-        virtual ~EventProcessor() {}
+        virtual ~EventProcessor() = default;
     };
 
     class IEventQueuePort {
     public:
         virtual void enqueue(std::shared_ptr<Event>&& event) = 0;
+        virtual ~IEventQueuePort() = default;
     };
 
     class IEventQueue : public IEventQueuePort {
     public:
         virtual void registerProcessor(std::unique_ptr<EventProcessor>&& processor) = 0;
         virtual void processEvents() = 0;
-        virtual ~IEventQueue() {}
+        ~IEventQueue() override = default;
     };
 
     class EventQueue : public IEventQueue {
@@ -35,10 +36,10 @@ namespace pong::world::events {
         std::queue<std::shared_ptr<Event>> events;
         std::vector<std::unique_ptr<EventProcessor>> processors;
     public:
-        EventQueue() {}
-        void registerProcessor(std::unique_ptr<EventProcessor>&& processor);
-        void enqueue(std::shared_ptr<Event>&& event);
-        void processEvents();
+        EventQueue() = default;
+        void registerProcessor(std::unique_ptr<EventProcessor>&& processor) override;
+        void enqueue(std::shared_ptr<Event>&& event) override;
+        void processEvents() override;
     };
 }
 
