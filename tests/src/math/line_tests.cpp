3c171c96b776834e75020e679f267f6826304ae4
Ascendise <rapperido@gmail.com>
2025-02-26 21:20:34 +0100

    created some tests for math stuff (ready to be defined)

---
+++ tests/src/math/line_tests.cpp
@@ -0,0 +1,19 @@
+#include <gtest/gtest.h>
+#include <math.hpp>
+
+namespace pong::math {
+
+TEST(LineTests, FindIntersect_Intersects_ShouldReturnIntersectionPoint) {
+  // Arrange
+
+  // Act
+  // Assert
+}
+
+TEST(Matrix2DTests, FindIntersect_Parallel_ShouldReturnNothing) {
+  // Arrange
+  // Act
+  // Assert
+}
+
+} // namespace pong::math
---
+++ tests/src/math/matrix2d_tests.cpp
@@ -0,0 +1,17 @@
+#include <gtest/gtest.h>
+
+namespace pong::math {
+
+TEST(Matrix2DTests, Invert_AnyMatrix_ShouldReturnInvertedMatrix) {
+  // Arrange
+  // Act
+  // Assert
+}
+
+TEST(Matrix2DTests, MultiplyWithVector_Any_ShouldReturnResultAsVector) {
+  // Arrange
+  // Act
+  // Assert
+}
+
+} // namespace pong::math
