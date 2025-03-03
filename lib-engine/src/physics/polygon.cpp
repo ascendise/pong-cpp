ebeaf095332f984a615a58d174c9f947b4560eb1
Ascendise <rapperido@gmail.com>
2025-03-03 19:15:43 +0100

    fix out of bounds access in clip

--- lib-engine/src/physics/polygon.cpp
+++ lib-engine/src/physics/polygon.cpp
@@ -16,12 +16,15 @@ Polygon Polygon::clip(const Polygon &clip) const {
   for (int i = 1; i <= clip.vertices.size(); i++) {
     math::Vector2D clipVertex1 = clip.vertices[i - 1];
     math::Vector2D clipVertex2 = clip.vertices[i];
+    if (i == clip.vertices.size())
+      clipVertex2 = clip.vertices[0];
     math::Line clipLine(clipVertex1, clipVertex2 - clipVertex1);
     for (int y = 1; y <= this->vertices.size(); y++) {
       math::Vector2D srcVertex1 = this->vertices[y - 1];
       math::Vector2D srcVertex2 = this->vertices[y];
-      math::Vector2D srcVector = srcVertex2 - srcVertex1;
-      math::Line srcLine(srcVertex1, srcVector);
+      if (y == this->vertices.size())
+        srcVertex2 = this->vertices[0];
+      math::Line srcLine(srcVertex1, srcVertex2 - srcVertex1);
       auto intersect = clipLine.findIntersect(srcLine);
       if (intersect.has_value()) {
         intersections.push_back(*intersect);
