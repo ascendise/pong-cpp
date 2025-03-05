5baf9a615094beafb1d88df5434163b5978f1658
Ascendise <rapperido@gmail.com>
2025-03-05 21:25:15 +0100

    i can clip two squares!

--- lib-engine/src/physics/polygon.cpp
+++ lib-engine/src/physics/polygon.cpp
@@ -12,24 +12,35 @@ Polygon::Polygon(const std::vector<math::Vector2D> &vertices) : vertices(vertice
 const std::vector<math::Vector2D> &Polygon::getVertices() const { return this->vertices; }
 
 Polygon Polygon::clip(const Polygon &clip) const {
-  std::vector<math::Vector2D> intersections;
+  std::vector<math::Vector2D> output = this->getVertices();
   for (int i = 0; i < clip.vertices.size(); i++) {
+    std::vector<math::Vector2D> input = output;
+    output.clear();
     math::Vector2D clipCurrent = clip.vertices[i];
     math::Vector2D clipLast = clip.vertices[(i - 1) % clip.vertices.size()];
     math::Line clipLine(clipCurrent, clipLast - clipCurrent);
-    for (int j = 0; j < this->vertices.size(); j++) {
-      math::Vector2D currentPoint = this->vertices[j];
-      math::Vector2D lastPoint = this->vertices[(j - 1) % this->vertices.size()];
+    for (int j = 0; j < input.size(); j++) {
+      math::Vector2D currentPoint = input[j];
+      math::Vector2D lastPoint = input[(j - 1) % input.size()];
       math::Line edge(currentPoint, lastPoint - currentPoint);
-      auto intersect = clipLine.findIntersect(edge);
-      if (!intersect.has_value())
-        continue;
+      auto intersect = clipLine.findIntersectInfinite(edge);
+      if (is_inside_edge(currentPoint, clipLast, clipCurrent)) {
+        if (!is_inside_edge(lastPoint, clipLast, clipCurrent))
+          output.push_back(*intersect);
+        output.push_back(currentPoint);
+      } else if (is_inside_edge(lastPoint, clipLast, clipCurrent)) {
+        output.push_back(*intersect);
+      }
     }
   }
-  return Polygon(intersections);
+  return Polygon(output);
 }
 
-bool Polygon::point_is_inside_polygon(const math::Vector2D &point, const math::Vector2D &edgePoint1,
-                                      const math::Vector2D &edgePoint2) const {}
+bool Polygon::is_inside_edge(const math::Vector2D &point, const math::Vector2D &edgePoint1,
+                             const math::Vector2D &edgePoint2) const {
+  math::Vector2D edgeVector = edgePoint2 - edgePoint1;
+  math::Vector2D pointVector = point - edgePoint1;
+  return pointVector.cross(edgeVector) <= 0;
+}
 
 } // namespace pong::physics
--- tests/src/physics/polygon_tests.cpp
+++ tests/src/physics/polygon_tests.cpp
@@ -30,14 +30,14 @@ TEST(PolygonTests, Polygon_CreateWithLessThanThreeVertices_ShouldThrowException)
 TEST(PolygonTests, Clip_ClippingPolygons_ShouldReturnClippingSection) {
   // Arrange
   Polygon square1(
-      {math::Vector2D(0.0f, 0.0f), math::Vector2D(0.0f, 2.0f), math::Vector2D(2.0f, 2.0f), math::Vector2D(2.0f, 0.0f)});
+      {math::Vector2D(0.0f, 0.0f), math::Vector2D(2.0f, 0.0f), math::Vector2D(2.0f, 2.0f), math::Vector2D(0.0f, 2.0f)});
   Polygon square2(
-      {math::Vector2D(1.0f, 1.0f), math::Vector2D(1.0f, 3.0f), math::Vector2D(3.0f, 3.0f), math::Vector2D(3.0f, 1.0f)});
+      {math::Vector2D(1.0f, 1.0f), math::Vector2D(3.0f, 1.0f), math::Vector2D(3.0f, 3.0f), math::Vector2D(1.0f, 3.0f)});
   // Act
   Polygon clip = square1.clip(square2);
   const std::vector<math::Vector2D> &actual = clip.getVertices();
-  std::vector<math::Vector2D> expected = {math::Vector2D(1.0f, 2.0f), math::Vector2D(2.0f, 2.0f),
-                                          math::Vector2D(2.0f, 1.0f), math::Vector2D(1.0f, 1.0f)};
+  std::vector<math::Vector2D> expected = {math::Vector2D(1.0f, 1.0f), math::Vector2D(2.0f, 1.0f),
+                                          math::Vector2D(2.0f, 2.0f), math::Vector2D(1.0f, 2.0f)};
   ASSERT_THAT(actual, expected);
 }
 
